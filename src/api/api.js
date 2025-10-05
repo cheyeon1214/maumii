59f10e5f694598724f124386765faf81b5491302
cheny <cheyeon001214@naver.com>
2025-10-05 23:39:43 +0900

    feat: .env.production ㅂ추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL/api || "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
   // baseURL: "http://localhost:9000/api", 
   withCredentials: true,   // 세션/쿠키 필요 없으면 제거해도 됨
   headers: { "Content-Type": "application/json" },
